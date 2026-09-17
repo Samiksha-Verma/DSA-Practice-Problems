57fa2fdc61d0f9a816a518cb9f6976fc745845a6
Samiksha-Verma <168643750+Samiksha-Verma@users.noreply.github.com>
2026-09-17 21:51:10 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.2%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n = s.size();
+        for(char ch: s){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+               char c = st.top();
+               st.pop();
+               if(ch==')' && c!='(' || ch =='}' && c!='{' || ch == ']' && c!='['){
+                return false;
+               }
+            }
+        }
+        return st.empty();
+    }
+};
